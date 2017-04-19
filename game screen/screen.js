dcb6a79f6e833aeeae5d7f6ec697a5846387aa6c
larson3 <larson3@umbc.edu>
2017-04-19 15:26:44 -0400

    Added some CSS changes and improved top bar

--- game screen/screen.js
+++ game screen/screen.js
@@ -1,6 +1,6 @@
 
 function startGame() {
-	console.log(document.getElementById("graphicsCanvas"));
+	//console.log(document.getElementById("graphicsCanvas"));
 	
 	class gameArea {
 		
@@ -10,7 +10,7 @@ function startGame() {
 		}
 		start(){
 		//clearrect on this.context
-			console.log(this.canvas);
+			//console.log(this.canvas);
 			this.context = this.canvas.getContext("2d");
 			this.interval = setInterval(updateGameArea, 20);
 		}
@@ -22,33 +22,53 @@ function startGame() {
 		
 		function updateGameArea() {
 			myGameArea.clear();
+			if(landmark.x == myGamePiece.x)
+			{
+				landmark.stop();
+			}
 			landmark.update();    
 			landmark.draw();
 			myGamePiece.draw();
 		}
 		var myGameArea = new gameArea();
-		var myGamePiece = new component(30, 30, "red", 575, 77.75,myGameArea);
-		var landmark = new component(20,60,"blue",15,50,myGameArea);
+		var myGamePiece = new component(40, 40, "wagon", 575, 77.75,myGameArea);
+		var landmark = new component(20,60,"landmark",15,50,myGameArea);
 		myGameArea.start();
 		loadMap();	
 }
 					
-		function component(width, height, color, x, y, myGameArea) {
+		function component(width, height, name, x, y, myGameArea) {
 			this.width = width;
 			this.height = height;
 			this.speedX = 1;
 			this.speedY = 0;
 			this.x = x;
 			this.y = y;    
+			this.name = name;
 			this.draw = function() {
 				ctx = myGameArea.context;
-				ctx.fillStyle = color;
+				
+			if(this.name == "wagon")
+			{
+				var wagon = new Image();
+				wagon.src = "pics/shittywagon.jpg";
+				ctx.drawImage(wagon,this.x,this.y,this.width, this.height);
+			}
+			else{
+				ctx.fillStyle = "blue";
 				ctx.fillRect(this.x, this.y, this.width, this.height);
 			}
+		}
+			
 			this.update = function() {
 				this.x += this.speedX;
 				this.y += this.speedY;        
 			}    
+			this.stop = function()
+			{
+				this.speedX = 0;
+				this.speedY = 0;
+			}
 		}
 					
 		
